fbe2328db72fd3daf6c01dacaab1170fdf11dd94
Charles <charlesnwoye2@gmail.com>
2024-06-13 12:21:02 +0100

    added notifications protobufs for reservations service

--- reservationService/proto-gen.sh
+++ reservationService/proto-gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python -m grpc_tools.protoc -I. --python_out=src/protobufs --grpc_python_out=src/protobufs reservation.proto
+python -m grpc_tools.protoc -I. --python_out=src/protobufs --grpc_python_out=src/protobufs *.proto
 
-python -m grpc_tools.protoc -I. --python_out=src/protobufs --grpc_python_out=src/protobufs inventory.proto
+# python -m grpc_tools.protoc -I. --python_out=src/protobufs --grpc_python_out=src/protobufs inventory.proto
